4bed309794ae36ae59b05e4c1ae786f4f2e72a7d
Hao Li <lihao921@gmail.com>
2015-02-02 00:05:28 +0800

    Use external cache

--- src/com/android/volley/toolbox/Volley.java
+++ src/com/android/volley/toolbox/Volley.java
@@ -40,7 +40,7 @@ public class Volley {
      * @return A started {@link RequestQueue} instance.
      */
     public static RequestQueue newRequestQueue(Context context, HttpStack stack) {
-        File cacheDir = new File(context.getCacheDir(), DEFAULT_CACHE_DIR);
+        File cacheDir = new File(context.getExternalCacheDir(), DEFAULT_CACHE_DIR);
 
         String userAgent = "volley/0";
         try {
